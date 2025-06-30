52c5b365585eb28859c9f405a8e263f23ace1588
macwqb <wuxiumu2@gmail.com;git config--global user.email wuxiumu2@gmail.com>
2025-06-30 14:25:44 +0800

    改成数据和页面分离的模式

---
+++ js/data/photo.html.js
@@ -0,0 +1,30 @@
+[
+    {
+        "date": "2025-05-05",
+        "title": "宝宝宴",
+        "desc": "第一次带着我们的龙凤宝贝回姥姥家办宝宝宴，小家伙们一路乖巧，让我感受到前所未有的幸福。姥姥姥爷的笑容中写满了满足，家人的欢声笑语温暖了整间屋子。人生最美好的样子，大概就是你们平安来到这个世界，被所有人爱着、疼着。感谢生命的恩赐，愿你们平安长大，每一步都幸福满满。",
+        "img": "https://jiatingshiguangxiangce.oss-cn-beijing.aliyuncs.com/20250505%E6%97%A0%E5%BF%A7%E6%97%A0%E8%99%91%E8%A7%81%E9%9D%A2%E4%BC%9A/3V8A1874%20%E6%8B%B7%E8%B4%9D.jpg?x-oss-process=image/resize,w_500/quality,q_60",
+        "link": "./20250505无忧无虑见面会.html"
+    },
+    {
+        "date": "2025-04-20",
+        "title": "宝宝第一次照相",
+        "desc": "看着镜头里的你们，眼里透着纯真与好奇，龙凤胎的小模样让我心都融化了。这是你们来到我们身边后的第一次正式拍照，每一帧都印刻着生命最初的美好。健康、快乐、平安成长，这是爸爸妈妈对你们最朴素的期盼。未来漫长，而我们，会一直陪伴，守护你们每一步的成长，愿你们一生无忧。",
+        "img": "https://jiatingshiguangxiangce.oss-cn-beijing.aliyuncs.com/20250420%E6%97%A0%E5%BF%A7%E6%97%A0%E8%99%91-%E7%99%BE%E5%A4%A9%E7%85%A7/8D7A6407.jpg?x-oss-process=image/resize,w_500/quality,q_60",
+        "link": "./20250420无忧无虑-百天照.html"
+    },
+    {
+        "date": "2023-12-15",
+        "title": "领证照",
+        "desc": "今天正式领证啦！带着彼此的承诺，我们开始了属于自己的小家。这一刻，红本本见证了我们甜蜜的誓言，照片里的笑容满是幸福与期待。从此以后，我们将共担风雨，共享阳光。愿未来的每一天，都充满温暖与理解，互相扶持着一路前行。谢谢你选择我，愿我们白头到老，恩爱如初，生活处处皆甜蜜。",
+        "img": "https://jiatingshiguangxiangce.oss-cn-beijing.aliyuncs.com/20231215%E9%A2%86%E8%AF%81/DSC05119.jpg?x-oss-process=image/resize,w_500/quality,q_60",
+        "link": "./20231215领证.html"
+    },
+    {
+        "date": "2023-09-18",
+        "title": "婚纱照",
+        "desc": "国庆节订婚后,又带着满满的仪式感，我们牵手拍了人生中第一组婚纱照。从陌生到相识，从相识到相知，每一步都珍藏着我们的故事。镜头前的微笑是真心的，彼此相望的眼神是坚定的。愿未来日子中，无论风雨或晴朗，我们都能携手共度，不负彼此深情，不负时光眷顾。此刻，幸福开始，未来，温暖同行。",
+        "img": "https://jiatingshiguangxiangce.oss-cn-beijing.aliyuncs.com/20230918%E5%A9%9A%E7%BA%B1%E7%85%A7/646A7955.jpg?x-oss-process=image/resize,w_500/quality,q_60",
+        "link": "./20230918婚纱照.html"
+    }
+]
